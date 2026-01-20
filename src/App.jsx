fb2d10d14c444aeb3df8ded02b68785dfa580152
SriJan <srijan1769@gmail.com>
2026-01-20 16:25:49 +0545

    ,,

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
-import './App.css'
-
+import "./App.css";
+import NavBar from "./Components/Navbar.jsx";
 function App() {
-  
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
-export default App
+export default App;
